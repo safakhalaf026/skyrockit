6e79b9a57c04a05abfea6e5db8a313a79c13c186
safakhalaf026 <safakhalaf026@gmail.com>
2025-12-09 10:03:50 +0300

    init

---
+++ server.js
@@ -0,0 +1,67 @@
+/* eslint-disable prefer-destructuring */
+const dotenv = require('dotenv');
+
+dotenv.config();
+const express = require('express');
+
+const app = express();
+// Sessions
+const session = require('express-session');
+const { MongoStore } = require('connect-mongo');
+
+// DB
+const mongoose = require('mongoose');
+
+// Middleware
+const methodOverride = require('method-override');
+const morgan = require('morgan');
+const passUserToView = require('./middleware/pass-user-to-view.js');
+const isSignedIn = require('./middleware/isSignedIn');
+
+// Controllers
+const authCtrl = require('./controllers/auth');
+
+// Set the port from environment variable or default to 3000
+const port = process.env.PORT ? process.env.PORT : '3000';
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+// Middleware to parse URL-encoded data from forms
+app.use(express.urlencoded({ extended: false }));
+// Middleware for using HTTP verbs such as PUT or DELETE
+app.use(methodOverride('_method'));
+// Morgan for logging HTTP requests
+app.use(morgan('dev'));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI,
+    }),
+  })
+);
+app.use(passUserToView);
+
+// Public
+app.get('/', async (req, res) => {
+  res.render('index.ejs');
+});
+
+app.use('/auth', authCtrl);
+
+// Protected Routes
+app.use(isSignedIn);
+
+app.get('/vip-lounge', async (req, res) => {
+  res.send('VIP PAGE');
+});
+
+app.listen(port, () => {
+  console.log(`The express app is ready on port ${port}!`);
+});
